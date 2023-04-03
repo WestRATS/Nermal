f5be721809ad4b69e6212fb2ea93e20912a548a7
Grace Pillai <828125969@myunit5.org>
2023-04-02 20:10:40 -0500

    format the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,55 @@
-import{useState} from 'react'
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
-import Form from './Form';
-import TaskForm from './task-tracking/components/TaskForm';
-import TaskList from './task-tracking/components/TaskList';
-import TaskFilter from './task-tracking/components/TaskFilter';
-import categories from './task-tracking/categories';
-import status from './task-tracking/status';
-
-
+import Form from "./Form";
+import TaskForm from "./task-tracking/components/TaskForm";
+import TaskList from "./task-tracking/components/TaskList";
+import TaskFilter from "./task-tracking/components/TaskFilter";
+import categories from "./task-tracking/categories";
+import status from "./task-tracking/status";
 
 function App() {
-const [selectedCategory, setSelectedCategory]= useState("")
+  const [selectedCategory, setSelectedCategory] = useState("");
 
-  const [tasks,setTasks] =  useState([
-    {id: 1, taskName: "math", status : "in progress", category : "school/work"},
-    {id: 6, taskName: "food bank", status : "tbd", category : "volunteer hours"},
-    {id: 4, taskName: "s.a.t", status : "in progress", category : "extra curriculars"}
-  ]
-  )
+  const [tasks, setTasks] = useState([
+    { id: 1, taskName: "math", status: "in progress", category: "school/work" },
+    {
+      id: 6,
+      taskName: "food bank",
+      status: "tbd",
+      category: "volunteer hours",
+    },
+    {
+      id: 4,
+      taskName: "s.a.t",
+      status: "in progress",
+      category: "extra curriculars",
+    },
+  ]);
 
-  const visibleTasks = selectedCategory ? tasks.filter(t => t.category === selectedCategory) : tasks
-  
-return (
-<div>
-  <div className="mb-5">
-    <TaskForm onSubmit={task => setTasks([...tasks, {...task, id:tasks.length + 1}])} />
-   
-  </div>
+  const visibleTasks = selectedCategory
+    ? tasks.filter((t) => t.category === selectedCategory)
+    : tasks;
 
-<TaskFilter onSelectCategory={category => setSelectedCategory(category)} />
+  return (
+    <div>
+      <div className="mb-5">
+        <TaskForm
+          onSubmit={(task) =>
+            setTasks([...tasks, { ...task, id: tasks.length + 1 }])
+          }
+        />
+      </div>
 
-<TaskList tasks={visibleTasks} onDelete={(id) => setTasks(tasks.filter(t => t.id != id))}></TaskList> 
+      <TaskFilter
+        onSelectCategory={(category) => setSelectedCategory(category)}
+      />
 
-
-
-</div>
-  )  
+      <TaskList
+        tasks={visibleTasks}
+        onDelete={(id) => setTasks(tasks.filter((t) => t.id != id))}
+      ></TaskList>
+    </div>
+  );
 }
 export default App;
